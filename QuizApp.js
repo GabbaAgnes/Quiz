65d8da6abb25bbeab2071056a27dca0609c9c0f7
Agnes <gabbaagnes@gmail.com>
2018-10-25 20:38:40 -0700

    final

--- QuizApp.js
+++ QuizApp.js
@@ -1,40 +1,72 @@
 const questions=[
     {
-        question:"what is 2+2?",
-        answer:"4",
+        question:"Which kind of animal did Florence Nightingale often carry around in her pocket?",
+        answer:"Owl",
         choices:[
-            "2","puppy","4","kitty",
+            "Kitten","Puppy","Owl","Snake",
         ]   
     },
     {
-        question:"what is 2+1?",
-        answer:"3",
+        question:"What does a nidoligist study?",
+        answer:"Bird Nests",
         choices:[
-            "2","3","dog","me",
+            "Waves","Clouds","Bird Nests","Caves",
         ]   
     },
     {
-        question:"what is 2+5?",
-        answer:"7",
+        question:"Hypermetropic people are what?",
+        answer:"Far-sighted",
         choices:[
-            "2","3","4","7",
+            "Obese","Underfed","Moody","Far-sighted",
+        ]   
+    }, 
+    {
+        question:"Anemophobia is the fear of what?",
+        answer:"Wind",
+        choices:[
+            "Spiders","The Dark","Fire","Wind",
+        ]   
+    }, 
+    {
+        question:"In which continent did the ostrich originate?",
+        answer:"Africa",
+        choices:[
+            "Africa","Australia","North America","Asia",
+        ]   
+    }, 
+    {
+        question:"For how long is a dog pregnant?",
+        answer:"9 Weeks",
+        choices:[
+            "9 Weeks","9 Days ","9 Months","9 Fortnights",
+        ]   
+    }, 
+    {
+        question:"What part of the body does a turtle use to breathe?",
+        answer:"Anus",
+        choices:[
+            "Belly","Shell","Mouth","Anus",
         ]   
     }, 
 ]    
+
+let score=0
 let num=0
 $("#quiz-form").on("submit", function(event){
     event.preventDefault();
-    
+    game="next"
     var answer=$('form input[type=radio]:checked').val();
     console.log(answer,questions[num].answer);
     $("#result").addClass("show")
-    if(answer == questions[num].answer){
-        $("#result p").text("You got it right!");
+    if(answer == questions[num].answer){ //answer it is correct
+        score++
+        $("#result p").text("You got it right! ♪~ ᕕ(ᐛ)ᕗ ");
     }
     else{
-        $("#result p").text(`You got it wrong! The right answer is ${questions[num].answer}`);
+        $("#result p").text(`You got it wrong!ᕙ(⇀‸↼‶)ᕗ The answer is ${questions[num].answer}`);
     }
     num++
+    $(".score").text(`Your score is ${score} out of ${num}`)
 });
     
 
@@ -51,42 +83,72 @@ $("#next").click(function(){
 
 
 function nextQuestion(){
-    
+    game="submit"
     $("#quiz-form").html(
         `<div id="question">${questions[num].question}</div>
         <fieldset>
-        <legend>We Animals</legend>
-         
-         
+        
             <label for="${questions[num].choices[0]}">${questions[num].choices[0]}</label>
-            <input type="radio" name="gender" id="${questions[num].choices[0]}" value="${questions[num].choices[0]}"><br>
+            <input tabindex="2" type="radio" name="gender" id="${questions[num].choices[0]}" value="${questions[num].choices[0]}"><br>
             <label for="${questions[num].choices[1]}">${questions[num].choices[1]}</label>
-            <input type="radio" name="gender" id="${questions[num].choices[1]}" value="${questions[num].choices[1]}"><br>
+            <input tabindex="-1" type="radio" name="gender" id="${questions[num].choices[1]}" value="${questions[num].choices[1]}"><br>
             <label for="${questions[num].choices[2]}">${questions[num].choices[2]}</label>
-            <input type="radio" name="gender" id="${questions[num].choices[2]}" value="${questions[num].choices[2]}"><br>
+            <input tabindex="-1" type="radio" name="gender" id="${questions[num].choices[2]}" value="${questions[num].choices[2]}"><br>
             <label for="${questions[num].choices[3]}">${questions[num].choices[3]}</label>
-            <input type="radio" name="gender" id="${questions[num].choices[3]}" value="${questions[num].choices[3]}"><br>
-            <input type="submit" value="Submit">
+            <input tabindex="-1" type="radio" name="gender" id="${questions[num].choices[3]}" value="${questions[num].choices[3]}"><br>
+            <input tabindex="-1" type="submit" value="Submit">
 
         </fieldset>
         `
     )
     $("#page").html(
-        `<p>Question number ${num+1}<\p>
+        `<p>Question number ${num+1} of 7<\p>
         `
     )
  
 }
 
-function endGame(){
-    alert("Game Over!")
-    num=0
-    nextQuestion()
-}
+let game="beginning"
 function beginGame(){
     nextQuestion()
     $("#welcome").removeClass("show") 
+    $("#page").addClass("show")
+    game="submit" 
 }
+
+function endGame(){
+    $("#end").addClass("show")
+    game="ended"
+} 
+
+function restartGame(){
+    $("#end").removeClass("show")
+    num=0
+    nextQuestion()
+    game="submit"
+}
+
 $("#begin").click(function(){
     beginGame()
 })
+
+$("#restart").click(function(){
+    restartGame()
+})
+/*$("body").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+        console.log("enterhit",game)
+        if (game=="beginning"){
+            $("#begin").click()
+        }
+        if (game=="next"){
+            $("#next").click()
+        }
+        if (game=="ended"){
+            $("#restart").click()
+        }
+        if (game=="submit"){
+            $("#quiz-form").submit()
+        }
+    }
+});*/
